aae84cdb3c6365da5be66e40cc74b49126866dc0
zcf0508 <zcf0508@live.com>
2021-09-01 22:28:36 +0800

    A 添加vue-router

---
+++ src/layout/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <img class="mx-auto" alt="Vue logo" src="@/assets/logo.png" />
+    <p>
+        <!--使用 router-link 组件进行导航 -->
+        <!--通过传递 `to` 来指定链接 -->
+        <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+        <router-link to="/home" class="mr-8">Go to Home</router-link>
+
+        <router-link to="/about">Go to About</router-link>
+    </p>
+    <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Layout',
+});
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { store, key } from '@/store';
+import router from '@/router';
 import './index.css';
 
-createApp(App).use(store, key).mount('#app');
+createApp(App).use(store, key).use(router).mount('#app');
