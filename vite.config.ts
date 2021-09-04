929fcc9c9de580e55996bd1ac4038d3af67b7ab1
zcf0508 <zcf0508@live.com>
2021-09-04 15:58:12 +0800

    A babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import * as path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        legacy({
+            targets: ['defaults', 'not IE 11'],
+        }),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
