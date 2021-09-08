e0e70af8f22f59e148943df3ddfd50bce8c81e7a
Zhou Caifa <caifa.zhou@naiveblue.com>
2021-09-08 11:53:44 +0800

    A gzip

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import * as path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import legacy from '@vitejs/plugin-legacy';
+import viteCompression from 'vite-plugin-compression'; //gzip
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
+        viteCompression({
+            threshold: 102400, //超过10k进行压缩
+        }),
         legacy({
             targets: ['defaults', 'not IE 11'],
         }),
