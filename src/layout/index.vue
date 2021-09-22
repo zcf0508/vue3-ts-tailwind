58d451a7eef11a6e3740aa000e89e9ede405ea60
Zhou Caifa <caifa.zhou@naiveblue.com>
2021-09-22 15:12:56 +0800

    A 添加@heroicons/vue图标依赖

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -4,18 +4,34 @@
         <!--使用 router-link 组件进行导航 -->
         <!--通过传递 `to` 来指定链接 -->
         <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
-        <router-link to="/home" class="mr-8">Go to Home</router-link>
+        <router-link to="/home" class="inline-block mr-8">
+            <div class="inline-block h-6">
+                <home-icon class="h-4 w-4 m-2"></home-icon>
+            </div>
+            <span>Go to Home</span>
+        </router-link>
 
-        <router-link to="/about">Go to About</router-link>
+        <router-link to="/about">
+            <div class="inline-block h-6">
+                <light-bulb-icon class="h-4 w-4 m-2"></light-bulb-icon>
+            </div>
+            <span>Go to About</span></router-link
+        >
     </p>
     <router-view></router-view>
 </template>
 
 <script lang="ts">
+import { HomeIcon } from '@heroicons/vue/outline';
+import { LightBulbIcon } from '@heroicons/vue/outline';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'Layout',
+    components: {
+        HomeIcon,
+        LightBulbIcon,
+    },
 });
 </script>
 
